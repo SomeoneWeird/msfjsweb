8a3d79fad6d9e99985d8640ddd9e02e93cd5de00
SomeoneWeird <adam@volatiletechnology.com>
2013-01-14 22:47:07 +1100

    Code import

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	
+	server: {
+		host: "127.0.0.1",
+		port: 8000
+	}
+
+}
---
+++ server.js
@@ -0,0 +1,49 @@
+var express = require('express'),
+		ejs 		= require('ejs'),
+		async 	= require('async'),
+		msfjs 	= require('msfjs')();
+
+var config = require('./config.js');
+
+var app = express();
+
+// Configure app
+
+app.use(express.static('files/public'));
+app.set('views', 'files/views');
+app.use(express.bodyParser());
+app.use(app.router);
+
+// Routes
+
+app.get('/', function(request, response) {
+
+		response.render('index.ejs', {
+			layout: false,
+			exploits: msfjs.Exploits.exploits,
+			payloads: msfjs.Payloads.payloads
+		});
+	
+});
+
+app.get('/exploits', function(request, response) {
+
+		response.render('exploits.ejs', {
+			layout: false,
+			exploits: msfjs.Exploits.exploits
+		});
+	
+});
+
+app.get('/payloads', function(request, response) {
+
+		response.render('payloads.ejs', {
+			layout: false,
+			payloads: msfjs.Payloads.payloads
+		});
+	
+});
+
+app.listen(config.server.port, config.server.host, function() {
+	console.log("Listening on " + [ config.server.host, config.server.port ].join(":"));
+});
