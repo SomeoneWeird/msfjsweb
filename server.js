6f51ec406132237a1e3b6c4ee0401f3ddb817c82
SomeoneWeird <adam@volatiletechnology.com>
2013-01-18 00:28:27 +1100

    you can now launch exploits!

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ var app = express();
 
 // Configure app
 
+app.use(express.bodyParser());
 app.use(express.static('files/public'));
 app.set('views', 'files/views');
-app.use(express.bodyParser());
 app.use(app.router);
 
 // Routes
@@ -46,11 +46,66 @@ app.get('/sessions', function(request, response) {
 
 });
 
+app.get('/sessions/:session', function(request, response) {
+
+	response.end("Not implemented yet, sorry!");
+
+});
+
 app.get('/payloads', function(request, response) {
 
 	response.render('payloads.ejs', {
 		layout: false,
 		payloads: msfjs.Payloads.payloads
+	});
+
+});
+
+app.get('/launch', function(request, response) {
+
+	response.render('launch.ejs', {
+		layout: false,
+		options: request.query
+	});
+
+});
+
+app.post('/launch', function(request, response) {
+
+	var options = {};
+
+	var exploit = request.body.exploit,
+			payload = request.body.payload,
+			target  = request.body.target;
+
+	options.rhost = target;
+
+	if(request.body.options) {
+
+		request.body.options.forEach(function(option) {
+			options[option.key] = option.value;
+		});
+
+	}
+
+	console.log("Launching " + payload + " at " + target + " using " + exploit);
+	
+	var exploit = new msfjs.Meterpreter(exploit, options);
+
+	msfjs.Exploits.launch(exploit, function(status) {
+
+		response.end();
+
+		status.on("success", function(session) {
+
+			console.log("Got meterpreter shell on " + session.connection.to.host);
+
+		});
+
+		status.on("error", function(error) {
+			console.error(error);
+		});
+
 	});
 
 });
